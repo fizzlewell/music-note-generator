d82959604415555ea034b8621b4f29b58864cade
Liu Wenzhong <Wenzhong@LiumatoMacBook-Pro.local>
2012-05-27 21:18:03 +0200

    Initial Commit

---
+++ music note generator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  music note generator
+//
+//  Created by Liu Wenzhong on 12-5-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
